02230787dd30a8d92092f85332b6b8dbfdac4eab
Michael Allan <mike@reluk.ca>
2021-02-16 21:34:25 -0500

    Clearer formatting.

--- Breccia.java
+++ Breccia.java
@@ -8,45 +8,42 @@ public final class Breccia {
 
 
 
-    /** Whether character `ch` is one that formally ends a newline.
+    /** Whether character  `ch` is one that formally ends a newline.
       * Returns true if `ch` is a line feed (A).
       */
     public static boolean completesNewline( final char ch ) { return ch == '\n'; }
 
 
-
     /** Whether code point `ch` is one that formally ends a newline.
       * Returns true if `ch` is a line feed (A).
       */
-    public static boolean completesNewline( final int ch ) { return ch == '\n'; }
+    public static boolean completesNewline( final  int ch ) { return ch == '\n'; }
 
 
 
-    /** Whether character `ch` is proper to a newline.
+    /** Whether character  `ch` is proper to a newline.
       * Returns true if `ch` is a line feed (A) or carriage return (D).
       */
     public static boolean impliesNewline( final char ch ) { return ch == '\n' || ch == '\r'; }
 
 
-
     /** Whether code point `ch` is proper to a newline.
       * Returns true if `ch` is a line feed (A) or carriage return (D).
       */
-    public static boolean impliesNewline( final int ch ) { return ch == '\n' || ch == '\r'; }
+    public static boolean impliesNewline( final  int ch ) { return ch == '\n' || ch == '\r'; }
 
 
 
-    /** Whether character `ch` is proper to a newline, yet does not formally complete it.
+    /** Whether character  `ch` is proper to a newline, yet does not formally complete it.
       * Returns true if `ch` is a carriage return (D).
       */
     public static boolean impliesWithoutCompletingNewline( final char ch ) { return ch == '\r'; }
 
 
-
     /** Whether code point `ch` is proper to a newline, yet does not formally complete it.
       * Returns true if `ch` is a carriage return (D).
       */
-    public static boolean impliesWithoutCompletingNewline( final int ch ) { return ch == '\r'; }
+    public static boolean impliesWithoutCompletingNewline( final  int ch ) { return ch == '\r'; }
 
 
 
@@ -56,15 +53,14 @@ public final class Breccia {
 
 
 
-    /** Whether character `ch` is a plain (20) or no-break space (A0).
+    /** Whether character  `ch` is a plain (20) or no-break space (A0).
       */
     public static boolean isSpace( final char ch ) { return ch == ' ' || ch == '\u00A0'; }
 
 
-
     /** Whether code point `ch` is a plain (20) or no-break space (A0).
       */
-    public static boolean isSpace( final int ch ) { return ch == ' ' || ch == '\u00A0'; }}
+    public static boolean isSpace( final  int ch ) { return ch == ' ' || ch == '\u00A0'; }}
 
 
 
