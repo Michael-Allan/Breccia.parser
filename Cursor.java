fb3e84cba1b860076630eca391962d14740b7613
Michael Allan <mike@reluk.ca>
2021-03-21 06:11:40 -0400

    Stiplulation against narrowing conversions.

--- Cursor.java
+++ Cursor.java
@@ -1,7 +1,11 @@
 package Breccia.parser;
 
+import java.lang.annotation.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 
 /** A unidirectional cursor over a series of discrete states reflecting parsed markup.
@@ -12,153 +16,153 @@ public interface Cursor {
     /** Returns the present parse state as an `AssociativeReference`,
       * or null if the cursor is not positioned at an associative reference.
       */
-    public AssociativeReference asAssociativeReference();
+    public @NarrowNot AssociativeReference asAssociativeReference();
 
 
 
     /** Returns the present parse state as an `AssociativeReference.End`,
       * or null if the cursor is not positioned at the end of an associative reference.
       */
-    public AssociativeReference.End asAssociativeReferenceEnd();
+    public @NarrowNot AssociativeReference.End asAssociativeReferenceEnd();
 
 
 
     /** Returns the present parse state as a `BodyFractum`,
       * or null if the cursor is not positioned at a body fractum.
       */
-    public BodyFractum asBodyFractum();
+    public @NarrowNot BodyFractum asBodyFractum();
 
 
 
     /** Returns the present parse state as a `BodyFractum.End`,
       * or null if the cursor is not positioned at the end of a body fractum.
       */
-    public BodyFractum.End asBodyFractumEnd();
+    public @NarrowNot BodyFractum.End asBodyFractumEnd();
 
 
 
     /** Returns the present parse state as a `CommandPoint`,
       * or null if the cursor is not positioned at a command point.
       */
-    public CommandPoint asCommandPoint();
+    public @NarrowNot CommandPoint asCommandPoint();
 
 
 
     /** Returns the present parse state as a `CommandPoint.End`,
       * or null if the cursor is not positioned at the end of a command point.
       */
-    public CommandPoint.End asCommandPointEnd();
+    public @NarrowNot CommandPoint.End asCommandPointEnd();
 
 
 
     /** Returns the present parse state as a `Division`,
       * or null if the cursor is not positioned at a division.
       */
-    public Division asDivision();
+    public @NarrowNot Division asDivision();
 
 
 
     /** Returns the present parse state as a `Division.End`,
       * or null if the cursor is not positioned at the end of a division.
       */
-    public Division.End asDivisionEnd();
+    public @NarrowNot Division.End asDivisionEnd();
 
 
 
     /** Returns the present parse state as `Empty`, or null if the markup source is not empty.
       */
-    public Empty asEmpty();
+    public @NarrowNot Empty asEmpty();
 
 
 
     /** Returns the present parse state as a `FileFractum`,
       * or null if the cursor is not positioned at a file fractum.
       */
-    public FileFractum asFileFractum();
+    public @NarrowNot FileFractum asFileFractum();
 
 
 
     /** Returns the present parse state as a `FileFractum.End`,
       * or null if the cursor is not positioned at the end of a file fractum.
       */
-    public FileFractum.End asFileFractumEnd();
+    public @NarrowNot FileFractum.End asFileFractumEnd();
 
 
 
     /** Returns the present parse state as a `Fractum`,
       * or null if the cursor is not positioned at a fractum.
       */
-    public Fractum asFractum();
+    public @NarrowNot Fractum asFractum();
 
 
 
     /** Returns the present parse state as a `Fractum.End`,
       * or null if the cursor is not positioned at the end of a fractum.
       */
-    public Fractum.End asFractumEnd();
+    public @NarrowNot Fractum.End asFractumEnd();
 
 
 
     /** Returns the present parse state as `Halt`,
       * or null if this cursor is not halted.
       */
-    public Halt asHalt();
+    public @NarrowNot Halt asHalt();
 
 
 
     /** Returns the present parse state as a `PlainCommandPoint`,
       * or null if the cursor is not positioned at a plain command point.
       */
-    public PlainCommandPoint asPlainCommandPoint();
+    public @NarrowNot PlainCommandPoint asPlainCommandPoint();
 
 
 
     /** Returns the present parse state as a `PlainCommandPoint.End`,
       * or null if the cursor is not positioned at the end of a plain command point.
       */
-    public PlainCommandPoint.End asPlainCommandPointEnd();
+    public @NarrowNot PlainCommandPoint.End asPlainCommandPointEnd();
 
 
 
     /** Returns the present parse state as a `PlainPoint`,
       * or null if the cursor is not positioned at a plain point.
       */
-    public PlainPoint asPlainPoint();
+    public @NarrowNot PlainPoint asPlainPoint();
 
 
 
     /** Returns the present parse state as a `PlainPoint.End`,
       * or null if the cursor is not positioned at the end of a plain point.
       */
-    public PlainPoint.End asPlainPointEnd();
+    public @NarrowNot PlainPoint.End asPlainPointEnd();
 
 
 
     /** Returns the present parse state as a `Point`,
       * or null if the cursor is not positioned at a point.
       */
-    public Point asPoint();
+    public @NarrowNot Point asPoint();
 
 
 
     /** Returns the present parse state as a `Point.End`,
       * or null if the cursor is not positioned at the end of a point.
       */
-    public Point.End asPointEnd();
+    public @NarrowNot Point.End asPointEnd();
 
 
 
     /** Returns the present parse state as a `Privatizer`,
       * or null if the cursor is not positioned at a privatizer.
       */
-    public Privatizer asPrivatizer();
+    public @NarrowNot Privatizer asPrivatizer();
 
 
 
     /** Returns the present parse state as a `Privatizer.End`,
       * or null if the cursor is not positioned at the end of a privatizer.
       */
-    public Privatizer.End asPrivatizerEnd();
+    public @NarrowNot Privatizer.End asPrivatizerEnd();
 
 
 
@@ -169,7 +173,7 @@ public interface Cursor {
       *     @throws NoSuchElementException If the present state
       *       {@linkplain ParseState#isFinal() is final}.
       */
-    public ParseState next() throws ParseError;
+    public @NarrowNot ParseState next() throws ParseError;
 
 
 
@@ -196,7 +200,19 @@ public interface Cursor {
       * those with {@linkplain Typestamp dedicated typestamps}.  Abstract states are present only
       * as alternative views of concrete states.  Each is got through a dedicated `as` method.
       */
-    public ParseState state(); }
+    public @NarrowNot ParseState state();
+
+
+
+   // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+
+    /** A warning not to cast the returned parse state to a subtype, or put it through
+      * any other narrowing conversion, as its narrower state may yet be unparsed.
+      * Always use the given `as` methods in lieu of narrowing conversions.
+      *
+      */ @Documented @Retention(SOURCE) @Target(METHOD)
+    public static @interface NarrowNot {}}
 
 
 
