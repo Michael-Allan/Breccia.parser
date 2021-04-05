1e6a5244a562e9aa3c398a3708d9e9c5bb00ef4d
Michael Allan <mike@reluk.ca>
2021-04-05 09:28:56 -0400

    Clarity.

--- TagName.java
+++ TagName.java
@@ -6,8 +6,8 @@ import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 
-/** Indicator of the tag name for a type of markup.  For each instance `m` of the type, if `m` belongs
-  * to no subtype of a contrary indication, then `m.tagName` will yield the indicated tag name.
+/** Indicant of the tag name for a type of markup.  For multi-typed markup with contrary indicants,
+  * use that of the most dervived type.  For markup with no indicant, use the generic name ‘Markup’.
   *
   *     @see Markup#tagName()
   */
