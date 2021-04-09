f117d50e3a14b6afb9bf5ba35a6d4640785121c4
Michael Allan <mike@reluk.ca>
2021-04-09 19:38:29 -0400

    Precision.

--- CommandPoint.java
+++ CommandPoint.java
@@ -14,6 +14,15 @@ public @DataReflector interface CommandPoint extends Point {
 
 
 
+   // ━━━  P o i n t  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+
+    /** The point descriptor.
+      */
+    public @Override Markup descriptor();
+
+
+
    // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 
 
